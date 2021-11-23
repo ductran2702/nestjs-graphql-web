fd2fc01d4dff255d851f323b592cfac1616b937f
ductran2702 <ductran2702@gmail.com>
2021-11-23 11:00:05 +0700

    feat: login with facebook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const handleClick=()=>{
+    window.FB.login(function(response) {
+        if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          window.FB.api('/me', function(response) {
+          console.log('Good to see you, ' + response.name + '.');
+         });
+        } else {
+          console.log('User cancelled login or did not fully authorize.');
+        }
+    });
+  }
+
+  const checkLoginState=()=>{
+    window.FB.getLoginStatus(function(response) {
+      console.log('getLoginStatus', response);
+    });
+  }
+
+  useEffect(() => {
+    window.fbAsyncInit = function() {
+      window.FB.init({
+          appId      : '997905604271695',
+          xfbml      : true,
+          version    : 'v0.1'
+        });
+      window.FB.AppEvents.logPageView();
+      
+    };
+
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    // 
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +60,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <button  onClick={handleClick}>Login</button>
+      <button  onClick={checkLoginState}>Check login</button>
+
     </div>
+    
   );
 }
 
